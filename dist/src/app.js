5ea16004e620aea22e92b5b9e4b6837b104122ae
DESKTOP-9VU9I7S\Rai <ajipunk008@gmail.com>
2023-07-17 19:58:25 +0700

    build

--- dist/src/app.js
+++ dist/src/app.js
@@ -10,17 +10,17 @@ const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const middlewares_1 = require("./middlewares");
 const api_1 = __importDefault(require("./api"));
+// @ts-ignore
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const swagger_1 = require("./config/swagger");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use((0, morgan_1.default)("dev"));
 app.use((0, helmet_1.default)());
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.get("/", (req, res) => {
-    res.json({
-        message: "unicornheadrainbow✨hi✨rainbowunicornhead",
-    });
-});
+// swagger
+app.use("/", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_1.swaggerConfig));
 // app.use(activityLogger);
 app.use("/api/v1", api_1.default);
 app.use(middlewares_1.notFound);
